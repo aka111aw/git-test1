e9f661acb115a2166bd3d5613b3954bcecd06b7a
Philwu <Philwu@123.com>
2023-01-05 13:03:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author PhilJo
+ * @create 2023-01-05 12:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
